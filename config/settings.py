14f1d2e20de5662c2e23a309a87d01a4bf787b56
damir <saskedir@mail.ru>
2024-05-24 15:38:51 +0300

    start work

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # myapps
+
+    'rest_framework',
+    'tasks',
 ]
 
 MIDDLEWARE = [
