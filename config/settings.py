2e00c434fc0a6647760d0ed93c2497bf4be01392
damir <saskedir@mail.ru>
2024-05-24 14:26:12 +0300

    init develop

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'USER': os.getenv('USER')
     }
 }
 
