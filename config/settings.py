533c51a9c5fead8156c79f8f43e18ead1279e5b6
Damir4234 <saskedir@mail.ru>
2024-06-04 22:14:52 +0300

    password is hashing

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'tasks',
-    'users'
+    'users',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
-
+from django.contrib.auth.hashers import make_password
 
 class CustomUser(AbstractUser):
     email = models.EmailField(
@@ -14,3 +14,8 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['username']
+
+    def save(self, *args, **kwargs):
+        if self.pk is None or 'password' in self.get_deferred_fields():
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
