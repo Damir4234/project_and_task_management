446f93fab054d6fb1535976d60c8e1d8647993b8
Damir4234 <saskedir@mail.ru>
2024-06-06 20:02:58 +0300

    fix password

--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from users.models import CustomUser 
 from users.serializers import UserSerializer
-
+from rest_framework.response import Response
 
 class UserCreate(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Переопределение метода для сохранения хешированного пароля в бд (если пароль не хешируется -
+        пользователь не считается активным и токен авторизации не создается)"""
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        password = serializer.data["password"]
+        user = CustomUser.objects.get(pk=serializer.data["id"])
+        user.set_password(password)
+        user.is_active = True
+        user.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
