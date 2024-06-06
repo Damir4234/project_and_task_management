caa5071327e1067d3f3dfc7de35a80da6b9a4f96
Damir4234 <saskedir@mail.ru>
2024-06-06 20:08:30 +0300

    password is hashing and get token

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     
     path('user/create/', UserCreate.as_view(), name='create_user'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
